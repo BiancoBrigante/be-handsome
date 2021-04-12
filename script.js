b7731792a9b9dade7ec3214131b128ee88579f3e
BB <n-angelo@bk.ru>
2021-04-12 18:19:13 +0300

    добавляем динамические записи

---
+++ script.js
@@ -0,0 +1,237 @@
+let arr = {
+    "tattoo": [
+        {
+            "title": "",
+            "description": "",
+            "autor": "",
+            "data": "",
+            "avatar": "",
+            "img": ""
+        },
+        {
+            "title": "Caesar lettering design",
+            "description": "This is my Caesar lettering design work and I'm really proud of it!",
+            "autor": "Dmitry A.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaDmitry.jpg",
+            "img": "./src/images/tattoo1.jpg"
+        },
+        {
+            "title": "Japan hanya",
+            "description": "Japan hanya for my best client!",
+            "autor": "Dmitry A.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaDmitry.jpg",
+            "img": "./src/images/tattoo2.jpg"
+        },
+        {
+            "title": "Tallinn",
+            "description": "Tallinn art tattoo. Isn't it perfect?",
+            "autor": "Dmitry A.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaDmitry.jpg",
+            "img": "./src/images/tattoo3.jpg"
+        },
+        {
+            "title": "Dream",
+            "description": "Simple. Beautiful. Strong.",
+            "autor": "Dmitry A.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaDmitry.jpg",
+            "img": "./src/images/tattoo4.jpg"
+        },
+        {
+            "title": "#minimal",
+            "description": "Simple and minimalistic dragon tattoo!",
+            "autor": "Dmitry A.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaDmitry.jpg",
+            "img": "./src/images/tattoo5.jpg"
+        },
+        {
+            "title": "Freestyle",
+            "description": "Another one freestyle tattoo!",
+            "autor": "Dmitry A.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaDmitry.jpg",
+            "img": "./src/images/tattoo6.jpg"
+        },
+        {
+            "title": "Caesar lettering design",
+            "description": "This is my Caesar lettering design work and I'm really proud of it!",
+            "autor": "Dmitry A.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaDmitry.jpg",
+            "img": "./src/images/tattoo7.jpg"
+        },
+        {
+            "title": "Caesar lettering design",
+            "description": "This is my Caesar lettering design work and I'm really proud of it!",
+            "autor": "Dmitry A.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaDmitry.jpg",
+            "img": "./src/images/tattoo8.jpg"
+        },
+        {
+            "title": "Caesar lettering design",
+            "description": "This is my Caesar lettering design work and I'm really proud of it!",
+            "autor": "Dmitry A.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaDmitry.jpg",
+            "img": "./src/images/tattoo9.jpg"
+        },
+        {
+            "title": "Caesar lettering design",
+            "description": "This is my Caesar lettering design work and I'm really proud of it!",
+            "autor": "Dmitry A.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaDmitry.jpg",
+            "img": "./src/images/tattoo10.jpg"
+        }
+    ],
+    "piercing": [
+        {
+            "title": "Пирсинг языка",
+            "description": "Простой и красивый пирсинг языка о котором вы всегда мечтали!",
+            "autor": "Sergey S.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaSergey.jpg",
+            "img": "./src/images/piercing1.jpg"
+        },
+        {
+            "title": "Eyebrow pierce",
+            "description": "Eyerrow pierce - it's simple and shiny.",
+            "autor": "Sergey S.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaSergey.jpg",
+            "img": "./src/images/piercing2.jpg"
+        },
+        {
+            "title": "Simple beauty",
+            "description": "Simple and beauty ear piercing",
+            "autor": "Sergey S.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaSergey.jpg",
+            "img": "./src/images/piercing3.jpg"
+        },
+        {
+            "title": "Dream",
+            "description": "Simple. Beautiful. Strong.",
+            "autor": "Sergey S.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaSergey.jpg",
+            "img": "./src/images/piercing4.jpg"
+        }
+    ],
+    "beauty": [
+        {
+            "title": "Тени",
+            "description": "Красивые тени для красивого вечера!",
+            "autor": "Elena M.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaElena.jpg",
+            "img": "./src/images/beauty1.jpg"
+        },
+        {
+            "title": "Stunning make-up",
+            "description": "Обворожительный макияж на любой случай.",
+            "autor": "Elena M.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaElena.jpg",
+            "img": "./src/images/beauty2.jpg"
+        },
+        {
+            "title": "Halloween",
+            "description": "Scary halloween make-up for ladies!",
+            "autor": "Elena M.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaElena.jpg",
+            "img": "./src/images/beauty3.jpg"
+        },
+        {
+            "title": "Dream",
+            "description": "Simple. Beautiful. Strong.",
+            "autor": "Elena M.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaElena.jpg",
+            "img": "./src/images/beauty4.jpg"
+        }
+    ],
+    "hair": [
+        {
+            "title": "Свадебная причёска",
+            "description": "Красота - в простоте!",
+            "autor": "Анна С.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaAnn.jpg",
+            "img": "./src/images/hair1.jpg"
+        },
+        {
+            "title": "Everyday routine",
+            "description": "Your hairstyle for every day!",
+            "autor": "Анна С.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaAnn.jpg",
+            "img": "./src/images/hair2.jpg"
+        },
+        {
+            "title": "Свадьба",
+            "description": "Ещё одна свадебная причёска, которая не оставит вас без внимания!",
+            "autor": "Анна С.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaAnn.jpg",
+            "img": "./src/images/hair3.jpg"
+        },
+        {
+            "title": "Dream",
+            "description": "Simple. Beautiful. Strong.",
+            "autor": "Анна С.",
+            "data": "Nov 12",
+            "avatar": "./src/images/avaAnn.jpg",
+            "img": "./src/images/hair4.jpg"
+        }
+    ]
+}
+
+const tattooTab = document.getElementById('tattooTab'),
+    pierceTab = document.getElementById('pierceTab'),
+    beautyTab = document.getElementById('beautyTab'),
+    hairTab = document.getElementById('hairTab'),
+    allTab = document.getElementById('allTab'),
+    feed = document.getElementById('feed');
+
+let showTag = () => {
+    tattooTab.onclick = () => {
+        console.log('Hey!')
+        feed.innerHTML = '';
+        for (let i = 1; i < arr.tattoo.length; i++) {
+            feed.innerHTML +=
+                `
+    <div class="col mb-5" style="max-width: 300px; max-height: 400px;">
+                <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg"
+                    data-bs-toggle="modal" data-bs-target="#card1Modal"
+                    style="background-size: cover; background-position: center; background-image: url('./src/images/tattoo${i}.jpg');">
+                    <div class="d-flex flex-column h-100 p-4 pb-3 text-white text-shadow-1">
+                        <h2 class="fs-3 lh-1 fw-bold"
+                            style="height: 300px; width:200px; text-shadow: rgb(0, 0, 114) 1px 1px 3px;">${arr.tattoo[i].title}</h2>
+                        <ul class="d-flex list-unstyled mt-auto">
+                            <li class="me-auto">
+                                <img src=${arr.tattoo[i].avatar} alt="avatar" width="40" height="40"
+                                    class="rounded-circle border border-white">
+                            </li>
+                            <ul class="d-flex fs-5 align-items-center me-3">
+                                <small style="text-shadow: black 1px 1px 2px;">${arr.tattoo[i].autor}</small>
+                            </ul>
+                            <li class="d-flex fs-5 align-items-center">
+                                <small style="text-shadow: black 1px 1px 2px;">${arr.tattoo[i].data}</small>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    `;
+        }
+    }
+}
+
+showTag()
